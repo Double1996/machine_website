ef32d8b4e45d69cb664030fe4a7042b49b9fd638
double <bud2@qq.com>
2017-06-12 14:01:04 +0800

    密码重置邮件中文信息

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -5,10 +5,10 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = "Account activated!"
+      flash[:success] = "账号激活"
       redirect_to user
     else
-      flash[:danger] = "Invalid activation link"
+      flash[:danger] = "激活不成功"
       redirect_to root_url
     end
   end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "账号激活"
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "Password reset"
+    mail to: user.email, subject: "密码重置"
   end
 end
