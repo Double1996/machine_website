419c1544cbe15ab5fa68af88509a8bfa7d6a8fc4
double <bud2@qq.com>
2017-06-17 01:28:34 +0800

    Seed创建管理员用户

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Users
+# 创建管理员用户
 User.create!(name:  "root",
              email: "root@qq.com",
              password:              "19961010",
@@ -6,30 +6,3 @@ User.create!(name:  "root",
              admin:     true,
              activated: true,
              activated_at: Time.zone.now)
-
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
-
-# Microposts
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
-
-# Following relationships
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
